d6f23e6b5b456253539014feb587dbaa88c8ca05
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-01-15 16:41:37 -0800

    code along

--- 04-Web-APIs/02-Homework/script.js
+++ 04-Web-APIs/02-Homework/script.js
@@ -1,5 +1,42 @@
 var timerEl = document.getElementById("fulltime");
 var startbtn = document.getElementById("startbtn");
+var questions = [
+    {
+    question: "What is git?",
+    choices: ["a. Version Control", "b. Git is the same as GitHub"],
+    answer: "a. Version Control",
+    },
+    {
+    question: "What is git?",
+    choices: ["a. Version Control", "b. Git is the same as GitHub"],
+    answer: "a. Version Control",
+    },
+    {
+    question: "What is git?",
+    choices: ["a. Version Control", "b. Git is the same as GitHub"],
+    answer: "a. Version Control",
+    },
+    {
+    question: "What is git?",
+    choices: ["a. Version Control", "b. Git is the same as GitHub"],
+    answer: "a. Version Control",
+    },
+    {
+    question: "What is git?",
+    choices: ["a. Version Control", "b. Git is the same as GitHub"],
+    answer: "a. Version Control",
+    },
+];
+
+
+for (let i = 0; i < questions.length; i++) {
+    questions.question;
+
+    questions[i].choices.forEach(function (choice){
+        console.log(choice);
+    })
+
+} 
 
 startbtn.addEventListener("click", function(){
 
