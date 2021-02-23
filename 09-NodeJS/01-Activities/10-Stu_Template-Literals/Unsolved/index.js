134273395574565108d71f674507140e3b273550
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-02-23 15:09:45 -0800

    activities

--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,13 +1,18 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
+  title: "Fight Me",
+  artist: "John Legend",
+  album: "Who knows",
 };
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
 const songSnippet = `
   <div class="song">
-
+  "${music.title}" by:
+  ${music.artist} - Album:
+  ${music.album}
   </div>
 `;
 
