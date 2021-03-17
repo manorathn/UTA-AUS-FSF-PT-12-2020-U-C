1035b3f868e6697f0b66c3b55bd241992a5f96d8
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-03-17 16:31:24 -0700

    updates

--- 12-MySQL/01-Activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
+++ 12-MySQL/01-Activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
@@ -3,25 +3,33 @@ DROP DATABASE IF EXISTS favorite_db;
 -- Creates the "favorite_db" database --
 CREATE DATABASE favorite_db;
 
--- Make it so all of the following code will affect favorite_db --
+-- Makes it so all of the following code will affect favorite_db --
+USE favorite_db;
 
 -- Creates the table "favorite_foods" within favorite_db --
 CREATE TABLE favorite_foods (
-  -- Make a string column called "food" which cannot contain null --
-  -- Make an numeric column called "score" --
+  -- Makes a string column called "food" which cannot contain null --
+  food VARCHAR(50) NOT NULL,
+  -- Makes an numeric column called "score" --
+  score INTEGER(10)
 );
 
 CREATE TABLE favorite_songs (
-  -- Make a string column called "song" which cannot contain null --
+   -- Make a string column called "song" which cannot contain null --
+  song VARCHAR(100) NOT NULL,
   -- Make a string column called "artist" --
-  -- Make an integer column called "score" --
+  artist VARCHAR(50),
+    -- Make an integer column called "score" --
   score INTEGER(10)
 );
 
 CREATE TABLE favorite_movies (
-  -- Create a numeric column called "id" which automatically increments and cannot be null --
-  -- Create a string column called "movie" which cannot be null --
-  -- Create a boolean column called "five_times" that sets the default value to false if nothing is entered --
-  -- Make an integer column called "score" --
-  -- Set the primary key of the table to id --
+  -- Creates a numeric column called "id" which automatically increments and cannot be null --
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  movie VARCHAR(100) NOT NULL,
+  -- Creates a boolean column called "five_times" that sets the default value to false if nothing is entered --
+  five_times BOOLEAN DEFAULT false,
+  score INTEGER(10),
+  -- Sets the primary key of the table to id --
+  PRIMARY KEY (id)
 );
--- 12-MySQL/01-Activities/04-programmingDB/Unsolved/programming_db_unsolved.sql
+++ 12-MySQL/01-Activities/04-programmingDB/Unsolved/programming_db_unsolved.sql
@@ -4,18 +4,33 @@ DROP DATABASE IF EXISTS programming_db;
 CREATE DATABASE programming_db;
 
 -- Use programming db for the following statements --
+USE programming_db;
+
+
 
 CREATE TABLE programming_languages(
   -- Create a numeric column called "id" which will automatically increment its default value as we create new rows. --
-
+  id INTEGER(11) auto_increment NOT NULL, 
   -- Create a string column called "language" --
-
+  language VARCHAR(20),
   -- Create an integer column called "rating" --
-
+  rating INTEGER(11), 
   -- Create a boolean column called "mastered" which will automatically fill --
+  mastered BOOLEAN DEFAULT true,
   -- with true when a new row is made and the value isn't otherwise defined. --
-
+  PRIMARY KEY(id)
   -- Set the id as this table's primary key
 );
 
 -- Create new example rows
+INSERT INTO programming_languages (language, rating)
+VALUES ("Java", 95);
+
+INSERT INTO programming_languages(language, rating)
+VALUES ("C++", 95);
+
+INSERT INTO programming_languages (language, rating)
+VALUES("JQuery", 96);
+
+INSERT INTO programming_languages (language, rating)
+VALUES ("Python", 96);	
