ea966e4c4339c878c60dbf3f9fdfd7978694290f
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-04-01 16:35:35 -0700

    updates

--- 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -1,7 +1,10 @@
 module.exports = {
-  format_time: (date) => {
+  format_date: (date) => {
     return date.toLocaleTimeString();
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
   // adds five years to the date, and formats it as M/D/YYYY
+  format_date: (date) => {
+    return date.setFullYear(date.getFullYear() + 5);
+  },
 };
