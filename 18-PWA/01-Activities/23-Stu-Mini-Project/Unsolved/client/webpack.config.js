1a8d0cbc3244b3eb42e05ca0e85d9f4ce4285b07
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-05-03 15:55:30 -0700

    update

--- 18-PWA/01-Activities/23-Stu-Mini-Project/Unsolved/client/webpack.config.js
+++ 18-PWA/01-Activities/23-Stu-Mini-Project/Unsolved/client/webpack.config.js
@@ -1,5 +1,43 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
 const config = {
-  // Add webpack configuration code here
+  mode: 'development',
+  entry: {
+    app: './assets/js/index.js',
+    favorites: './assets/js/favorites.js',
+    topic: './assets/js/topic.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Newsy app',
+      short_name: 'Newsy',
+      description: 'An application that allows you to view different news articles and save your favorites.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+    }),
+  ],
 };
 
 module.exports = config;
--- 18-PWA/01-Activities/23-Stu-Mini-Project/Unsolved/server.js
+++ 18-PWA/01-Activities/23-Stu-Mini-Project/Unsolved/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 
 app.use(express.static("client"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +9,6 @@ app.use(express.json());
 
 require("./routes/htmlRoutes.js")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Now listening on port: ${PORT}`);
 });
